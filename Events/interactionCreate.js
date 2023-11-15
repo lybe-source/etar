ca63054415656c88ee0c3911b67f47bc92c24baa
Lybe <s3iiryo@live.be>
2023-11-15 12:58:54 +0100

    Feat: last commit for ticket command

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -36,34 +36,12 @@ module.exports = async (bot, interaction) => {
                 name: `ticket-${interaction.user.username}`,
                 type: Discord.ChannelType.GuildText
             });
+            
+            await channel.setParent(interaction.channel.parent.id); // ajoute le channel ticket dans la catégorie où se trouve le channel pour le créer
 
             await channel.permissionOverwrites.create(interaction.guild.roles.everyone, {
                 ViewChannel: false
             });
-
-            // Need a boucle to put false for ViewChannel at all roles
-            // await channel.permissionOverwrites.create([
-            //     { id: "1173617417914699887", deny: [ViewChannel] }, // Samouraï
-            //     { id: "1170909161010241637", deny: [ViewChannel] }, // Dev
-
-            //     { id: "1170877128414351401", deny: [ViewChannel] }, // All-Games
-            //     { id: "1170887655127527475", deny: [ViewChannel] }, // WoW
-            //     { id: "1170888611403665458", deny: [ViewChannel] }, // Wakfu
-            //     { id: "1170888950198575256", deny: [ViewChannel] }, // Civilization
-            //     { id: "1172569125244125348", deny: [ViewChannel] }, // SoT
-
-            //     { id: "1170913046508609596", deny: [ViewChannel] }, // PHP
-            //     { id: "1170913426223144960", deny: [ViewChannel] }, // Symfony
-            //     { id: "1170913929292161054", deny: [ViewChannel] }, // Laravel
-            //     { id: "1170914248264781965", deny: [ViewChannel] }, // Javascript
-            //     { id: "1170914601286766663", deny: [ViewChannel] }, // Typescript
-            //     { id: "1170914922356543525", deny: [ViewChannel] }, // React
-            //     { id: "1170915162836975617", deny: [ViewChannel] }, // Angular
-            //     { id: "1170915400519798885", deny: [ViewChannel] }, // VueJS
-            //     { id: "1170915667290103928", deny: [ViewChannel] }, // Java
-            //     { id: "1170915936631529534", deny: [ViewChannel] }, // Python
-            //     { id: "1170916183952851035", deny: [ViewChannel] }, // Ruby
-            // ])
             await channel.permissionOverwrites.create(interaction.user, {
                 ViewChannel: true,
                 SendMessages: true,
@@ -79,7 +57,6 @@ module.exports = async (bot, interaction) => {
                 EmbedLinks: true,
             });
 
-            await channel.setParent(interaction.channel.parent.id); // ajoute le channel ticket dans la catégorie où se trouve le channel pour le créer
             await channel.setTopic(interaction.user.id);
             await interaction.reply({content: `Votre ticket a correctement été créé : ${channel}`, ephemeral: true});
 
@@ -104,7 +81,7 @@ module.exports = async (bot, interaction) => {
 
         if (interaction.customId === "close") {
 
-            let user = bot.guilds.cache.get(interaction.channel.topic);
+            let user = bot.users.cache.get(interaction.channel.topic);
 
             try { await user.send("Votre ticket a été fermé !"); } catch (err) {}
 
